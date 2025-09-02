4b2df834d326ce878a136f8828d61177280e2a26
EmilioJOM <emijesus21@gmail.com>
2025-09-02 01:24:05 -0300

    Categories

--- src/main/java/com/uade/tpo/demo/controllers/categories/CategoriesController.java
+++ src/main/java/com/uade/tpo/demo/controllers/categories/CategoriesController.java
@@ -1,8 +1,6 @@
 package com.uade.tpo.demo.controllers.categories;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.uade.tpo.demo.entity.Category;
 import com.uade.tpo.demo.exceptions.CategoryDuplicateException;
@@ -14,10 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("categories")
@@ -42,6 +36,15 @@ public class CategoriesController {
             return ResponseEntity.ok(result.get());
 
         return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{categoryId}")
+    public ResponseEntity<Object> deleteCategoryById(@PathVariable Long categoryId) {
+        Boolean result = categoryService.deleteCategoryById(categoryId);
+        if (result)
+            return ResponseEntity.ok().build();
+
+        return ResponseEntity.notFound().build();
     }
 
     @PostMapping
